1eeb81587f9232b5bf4ebb73a89f8c3fd8790958
HAICHUN WANG <h.wang1.23@abdn.ac.uk>
2024-03-13 12:04:59 +0000

    requirement

---
+++ Assessment reqirement.py
@@ -0,0 +1,68 @@
+#1. to create a database driven Django application with at least two linked tables. [10 marks]'''
+'''3 for Django app started and not working with one table, 
+5 points for working with two or more linked tables, 
+7, (list plus item pages), 
+10 points for doing something more'''
+#We have to creat at least 2 tables and link them
+
+#2. to base your application on your provided open data source, and to display data by appropriate means.[10 marks]
+'''3 for displaying open data, 
+5 for comparing open data, 
+7 using visual tools, 
+10 for analysis doing something more'''
+#Displaying, comparing, visual and analysis, actually requires 2 function of the website，comparing and analysis
+
+#3. to load your open data of 2000-7000 records into the app in a suitable manner. [10 marks]
+'''Attempt to load data, 3; 
+5 for loading some data; 
+7 points for loading linked tables; 
+10 for more'''
+#API_EG.ELC.RNEW.ZS_DS2_en_excel_v2_6299116.xls
+
+#4. to use appropriate templates for the pages in your app. [10 marks]
+'''3 for attempted templates, 
+5 with some templates, 
+7 points with composite templates, 
+10 more'''
+#We have to made composite templates, link templates
+
+#5. to use appropriate error handling in your application. [10 marks]
+'''3 for attempted error handling, 
+5 for some error handling, 
+7 for effective use, 
+10 for more'''
+#Exception handling or 404 page?I am not sure
+
+#6. to provide a good code base, making use of different application components. [10 marks]
+'''3 for basic django app, 
+5 points for some repetition, 
+7 for code in appropriate files, 
+10 for more'''
+#Put the file into suitable folder
+
+#7. to use Git as part of your development process for source control. [10 marks] 
+'''3 git used, 
+5 points git log, 
+7 git log showing integration, 
+10 for more'''
+#commit with remark
+
+#8. to include suitable tests for your codebase. [10 marks]
+'''3 for basic tests, 
+5 points for wider use of tests, 
+7 for good coverage with tests, 
+10 for more'''
+#I only know unittest, I think we should use other test
+
+#9. to publish the application with a cloud provider. [10 marks]
+'''3 for attempted cloud deploy, 
+5 deployed to cloud, 
+7 deploy with mysql/postgresql, 
+10 for more'''
+#We can deploye it on ddashboard.render, but it said to deploy with mysql/postgresql? I am not sure
+
+'''10. to document the design, development, implementation, 
+installation and use of the application in theform of a one-page final report, 
+and in the readme.md file of the repository. [10 marks]'''
+#3 points for document plus readme file, 5 why & how app developed, 7 user details and cloud url, 10 formore
+#one-page report, 5 paragraphs，it's easy. in the readme.md with url.
